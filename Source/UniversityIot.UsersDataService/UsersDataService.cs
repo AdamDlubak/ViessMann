48146d00e1c51588c3172f98e877309fe8510ce6
SHERCOMP\Adam <asdas>
2016-12-18 00:58:23 +0100

    Init Lab 3

---
+++ Source/UniversityIot.UsersDataService/UsersDataService.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Entity;
+
+namespace UniversityIot.UsersDataService
+{
+    using System.Threading.Tasks;
+    using UsersDataAccess;
+    using UsersDataAccess.Models;
+
+    public class UsersDataService : IUsersDataService
+    {
+        public async Task<User> AddUserAsync(User user)
+        {
+            using (var context = new UsersContext())
+            {
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+                return user;
+            }
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            using (var context = new UsersContext())
+            {
+                var user = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (user != null)
+                {
+                    context.Users.Remove(user);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task<User> GetUserAsync(int id)
+        {
+            using (var context = new UsersContext())
+            {
+                var user = await context.Users.Include(x => x.UserGateways).FirstOrDefaultAsync(x => x.Id == id);
+
+                return user;
+            }
+        }
+
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            using (var context = new UsersContext())
+            {
+                context.Users.Attach(user);
+                context.Entry(user).State = EntityState.Modified;
+
+                await context.SaveChangesAsync();
+
+                return user;
+            }
+        }
+    }
+}
